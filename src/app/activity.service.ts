66e077f3f01fcfcdd29ba4fc73777fc5f9948349
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2020-05-10 09:26:37 +0300

    define activity interface and observables

--- src/app/activity.service.ts
+++ src/app/activity.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { Activity } from './types';
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +10,12 @@ import { HttpClient } from "@angular/common/http";
 export class ActivityService {
   constructor(private _httpCient: HttpClient) { }
 
-  getActivity(activityId: string){
-    return this._httpCient.get(`${API_URL}/id/${activityId}`);
+  getActivity(activityId: string): Observable<Activity> {
+    return this._httpCient.get<Activity>(`${API_URL}/id/${activityId}`);
   }
 
-  getAllActivity(){
-    return this._httpCient.get(API_URL);
+  getAllActivities(): Observable<Activity[]>{
+    return this._httpCient.get<Activity[]>(API_URL);
   }
 }
 
