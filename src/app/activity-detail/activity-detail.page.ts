3bfef0937269eaddc9e93a901622e78bda4dd7fc
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2020-05-13 02:54:23 +0300

    retrive details data

--- src/app/activity-detail/activity-detail.page.ts
+++ src/app/activity-detail/activity-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Activity } from '../types';
+import { ActivityService } from '../activity.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-activity-detail',
@@ -6,8 +10,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activity-detail.page.scss'],
 })
 export class ActivityDetailPage implements OnInit {
+  activityDetail: Observable<Activity>;
 
-  constructor() { }
+  constructor(activityService: ActivityService, activatedRoute: ActivatedRoute) {
+    const activityId = activatedRoute.snapshot.params["activityId"];
+    this.activityDetail = activityService.getActivity(activityId);
+   }
 
   ngOnInit() {
   }
