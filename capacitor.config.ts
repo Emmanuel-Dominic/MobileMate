e7b1458601e3046ada6d3b8d11489252018a4e94
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-10-31 16:24:28 +0300

    configure Capacitor and Ionic project for Appflow builds

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MobileMate',
+  webDir: 'www'
+};
+
+export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYLkFYud101RPDcW0ac8zEc4BOwgn02NU",
+    authDomain: "mobilemate-8ed3c.firebaseapp.com",
+    projectId: "mobilemate-8ed3c",
+    databaseURL: "https://mobilemate-8ed3c.firebaseio.com",
+    storageBucket: "mobilemate-8ed3c.appspot.com",
+    messagingSenderId: "275307020304",
+    appId: "1:275307020304:web:8474878c40150b07c30a95",
+    measurementId: "G-CT2GJ5MSNP"
+  }
 };
